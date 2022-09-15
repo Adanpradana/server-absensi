9e802d51783512bfe50705db924f1e1d69cd4544
Adanpradana <adanizkahariz@gmail.com>
2022-09-16 00:57:16 +0700

    fixing function routes

--- index.js
+++ index.js
@@ -3,22 +3,33 @@ const cors = require("cors");
 const app = express();
 const employee = require("./employee.json");
 const datas = employee.GRID.ROWS.ROW;
+
 const router = express.Router();
 app.use(cors());
 
 router.get("/", (req, res) => {
-  return res.json(datas);
+  return res.status(200).json(employee);
 });
 
-router.get("/employee/details", (req, res) => {
-  const user = datas.map((response) => response._attributes);
-  return res.status(200).json(user);
+// router.get("/main-api/v1", (req, res) => {
+//   return res.status(200).json(employee);
+// });
+// router.get("/main-api/v1/employee", (req, res) => {
+//   const user = datas.map((response) => response._attributes);
+//   return res.status(200).json(user);
+// });
+router.get("/main-api/v1/employee", (req, res) => {
+  for (let data of datas) return res.status(200).json(data._attributes);
 });
 
-router.get("/employee/details/:id", (req, res) => {
-  const id = req.params.id;
-  const user = datas.filter((data) => data._attributes.dbg_pegawaipegawai_nip == id);
-  return res.status(200).json(user);
+router.get("/main-api/v1/employee/:id", (req, res) => {
+  const { id } = req.params;
+  for (let data of datas) {
+    if (data._attributes.dbg_pegawaipegawai_nip == id) {
+      return res.status(200).json(data._attributes);
+    }
+  }
+  return res.status(400);
 });
 
 app.use(router);
